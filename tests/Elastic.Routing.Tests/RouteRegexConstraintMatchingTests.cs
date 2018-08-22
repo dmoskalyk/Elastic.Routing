69f652ab9727ec56e954e4a970270d239feee69f
Oleksii Biliachenko <o.biliachenko@sana-commerce.com>
2018-08-22 10:51:07 +0300

    Cover URL matching with tests which use regex constraint

---
+++ tests/Elastic.Routing.Tests/RouteRegexConstraintMatchingTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Routing;
+using System.Threading;
+using Moq;
+
+namespace Elastic.Routing.Tests
+{
+    [TestClass]
+    public class RouteRegexConstraintMatchingTests : ElasticRouteTestBase
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            InitializeBase();
+            request.SetupGet(c => c.RequestContext).Returns(() => new Mock<RequestContext>().Object);
+        }
+
+        const string fourLettersLanguage = "([a-zA-Z]{2})(-([a-zA-Z]{2}))?";
+
+        [TestMethod]
+        public void ElasticRoute_GetRouteData_OptionalRegexParameter_IsAddedToRouteValues()
+        {
+            var routeCollection = new RouteCollection()
+            {
+                new ElasticRoute("({lang}/)({groupId}/){id}",
+                    routeHandler: routeHandler,
+                    constraints: new { lang = new TestRegexConstraint(fourLettersLanguage) }
+                    )
+            };
+
+            request.SetupGet(r => r.AppRelativeCurrentExecutionFilePath).Returns("~/en-us/123");
+            var routeData = routeCollection.GetRouteData(context.Object);
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual(2, routeData.Values.Count);
+            Assert.AreEqual("en-us", routeData.Values["lang"]);
+            Assert.AreEqual("123", routeData.Values["id"]);
+        }
+
+        [TestMethod]
+        public void ElasticRoute_GetRouteData_OptionalRegexParameter_IsNotAddedToRouteValues()
+        {
+            var routeCollection = new RouteCollection()
+            {
+                new ElasticRoute("({lang}/)({groupId}/){id}",
+                    routeHandler: routeHandler,
+                    constraints: new { lang = new TestRegexConstraint(fourLettersLanguage) }
+                    )
+            };
+
+            request.SetupGet(r => r.AppRelativeCurrentExecutionFilePath).Returns("~/t-est/123");
+            var routeData = routeCollection.GetRouteData(context.Object);
+
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual(2, routeData.Values.Count);
+            Assert.AreEqual("t-est", routeData.Values["groupId"]);
+            Assert.AreEqual("123", routeData.Values["id"]);
+        }
+
+        [TestMethod]
+        public void ElasticRoute_GetRouteData_OptionalDynamicRegexParameter_AddedToRouteValuesAfterUpdate()
+        {
+            var constraint = new TestRegexConstraint(fourLettersLanguage);
+            var routeCollection = new RouteCollection()
+            {
+                new ElasticRoute("({lang}/)({groupId}/){id}",
+                    routeHandler: routeHandler,
+                    constraints: new { lang = constraint }
+                    )
+            };
+            request.SetupGet(r => r.AppRelativeCurrentExecutionFilePath).Returns("~/kok/123");
+
+            var routeData = routeCollection.GetRouteData(context.Object);
+
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual(2, routeData.Values.Count);
+            Assert.AreEqual("kok", routeData.Values["groupId"]);
+            Assert.AreEqual("123", routeData.Values["id"]);
+
+            constraint.UpdatePattern("([a-zA-Z]{2}|kok)(-([a-zA-Z]{2}))?");
+            routeData = routeCollection.GetRouteData(context.Object);
+
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual(2, routeData.Values.Count);
+            Assert.AreEqual("kok", routeData.Values["lang"]);
+            Assert.AreEqual("123", routeData.Values["id"]);
+        }
+    }
+}
---
+++ tests/Elastic.Routing.Tests/TestRegexConstraint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Routing;
+
+namespace Elastic.Routing.Tests
+{
+    public class TestRegexConstraint : IDynamicRegexRouteConstraint
+    {
+        public TestRegexConstraint(string pattern)
+        {
+            Pattern = pattern;
+        }
+
+        public void UpdatePattern(string newPattern)
+        {
+            Pattern = newPattern;
+            RegexChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public string Pattern { get; private set; }
+
+        public event EventHandler RegexChanged;
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+            => true;
+    }
+}
